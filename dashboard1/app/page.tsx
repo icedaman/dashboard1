fa2a9fe3775b78cbe3e9cbff1ea7625f1e87b154
icedaman <36274654+icedaman@users.noreply.github.com>
2024-08-02 06:02:49 +0100

    app scafolding

---
+++ dashboard1/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-red-800 font-bold text-4xl">HOME</h1>
+      <Button>
+        click me
+      </Button>
+    </div>  
+  );
+}
