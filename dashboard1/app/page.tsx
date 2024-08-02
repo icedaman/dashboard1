80d2cc49eadae033b40456f20f19b63cc2e95c4d
icedaman <36274654+icedaman@users.noreply.github.com>
2024-08-02 06:14:19 +0100

    added dark theme

--- dashboard1/app/page.tsx
+++ dashboard1/app/page.tsx
@@ -2,8 +2,7 @@ import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-red-800 font-bold text-4xl">HOME</h1>
+    <div className="flex h-screen max-h-screen">
       <Button>
         click me
       </Button>
